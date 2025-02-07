f237a2a8e701119c525f4311b8e7312674a1c2a5
downloader99 <akashshigvan108@gmail.com>
2025-02-07 19:08:50 +0530

    Update server.js

--- public/server.js
+++ public/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html when visiting "/"
+// Serve index.html for the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
